817aa167b3e2439aa2528a4cbf57317916d45607
HenryGBC <henry.bravo89@gmail.com>
2016-06-18 00:39:12 -0430

    First component

--- src/app/a2-force-awakens.component.ts
+++ src/app/a2-force-awakens.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import {HeroesComponent } from './heroes/';
 
 @Component({
   moduleId: module.id,
   selector: 'a2-force-awakens-app',
   templateUrl: 'a2-force-awakens.component.html',
-  styleUrls: ['a2-force-awakens.component.css']
+  styleUrls: ['a2-force-awakens.component.css'],
+  directives: [HeroesComponent]
 })
 export class A2ForceAwakensAppComponent {
   title = 'a2-force-awakens works!';
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  moduleId: module.id,
+  selector: 'toh-heroes',
+  templateUrl: 'heroes.component.html',
+  styleUrls: ['heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() {}
+
+  ngOnInit() {
+  }
+
+}
